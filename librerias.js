15d39cc7f8d972faf3d259613fe49ca4aa4a66fe
Vianey Chavez <viantage@gmail.com>
2024-09-26 20:59:33 -0600

    extraccion_gmaps

---
+++ librerias.js
@@ -0,0 +1,95 @@
+var libreriasData = { 'type':'FeatureCollection',
+'features':[
+ {'type':'Feature', 
+ 'properties':{'nombre':'Fondo de Cultura Econ√≥mica Librer√≠a Octavio Paz'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18266544,19.34735833]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠as Gandhi'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.16325155,19.34850661]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠as El S√≥tano'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.17989794,19.34637799]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠as Gandhi'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18226296,19.34733084]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠a Novo'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18117382,19.34316372]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'U-T√≥picas: Libros y caf√©'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.1636287,19.34631048]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠as Gandhi'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18028071,19.34570384]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Centro Cultural Elena Garro'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.15871489,19.34853753]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠a Jaime Garc√≠a Terr√©s, entrada CU'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18665351,19.33640295]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'El Gato Literato'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18046896,19.3358784]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠a Ahuizote'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18333449,19.34766645]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'LIBRER√çA MALINALLI'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.16843674,19.34550044]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠a Las Tres Cruces'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.16499153,19.34596718]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠a Siglo XXI'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.17665347,19.33445104]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Librer√≠a Miguel √Ångel Porr√∫a'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.19152796,19.34559322]}} 
+]};var libreriasData = { 'type':'FeatureCollection',
+'features':[
+ {'type':'Feature', 
+ 'properties':{'nombre':'Fondo de Cultura EconÛmica LibrerÌa Octavio Paz'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18266544,19.34735833]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌas Gandhi'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.16325155,19.34850661]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌas El SÛtano'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.17989794,19.34637799]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌas Gandhi'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18226296,19.34733084]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌa Novo'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18117382,19.34316372]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'U-TÛpicas: Libros y cafÈ'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.1636287,19.34631048]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌas Gandhi'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18028071,19.34570384]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'Centro Cultural Elena Garro'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.15871489,19.34853753]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌa Jaime GarcÌa TerrÈs, entrada CU'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18665351,19.33640295]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'El Gato Literato'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18046896,19.3358784]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌa Ahuizote'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.18333449,19.34766645]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LIBRERÕA MALINALLI'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.16843674,19.34550044]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌa Las Tres Cruces'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.16499153,19.34596718]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌa Siglo XXI'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.17665347,19.33445104]}} 
+]}; {'type':'Feature', 
+ 'properties':{'nombre':'LibrerÌa Miguel ¡ngel Porr˙a'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.19152796,19.34559322]}} 
+]};
